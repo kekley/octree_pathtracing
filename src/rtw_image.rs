08ddc1a6af106ac9bae0cb463c1e6d70ef26d6a4
kekley <jesusmacias8568@gmail.com>
2024-11-19 08:09:29 -0800

    textures!!!!!!!!!!!!!

---
+++ src/rtw_image.rs
@@ -0,0 +1,87 @@
+use stb_image::image::load;
+#[derive(Debug)]
+pub struct RTWImage {
+    bytes_per_pixel: u32,
+    fdata: Vec<f32>,
+    bdata: Vec<u8>,
+    pub image_width: u32,
+    pub image_height: u32,
+    bytes_per_scanline: u32,
+}
+
+impl RTWImage {
+    pub fn load(file_path: &str) -> Self {
+        let mut tmp = Self {
+            bytes_per_pixel: 0,
+            fdata: vec![],
+            bdata: vec![],
+            image_width: 0,
+            image_height: 0,
+            bytes_per_scanline: 0,
+        };
+        let load_result = load(file_path);
+        match load_result {
+            stb_image::image::LoadResult::Error(e) => panic!("{}", e),
+            stb_image::image::LoadResult::ImageU8(image) => {
+                tmp.bdata = image.data;
+                tmp.image_width = image.width as u32;
+                tmp.image_height = image.height as u32;
+                tmp.bytes_per_pixel = image.depth as u32;
+                tmp.bytes_per_scanline = tmp.image_width * tmp.bytes_per_pixel;
+                tmp.convert_to_floats();
+            }
+            stb_image::image::LoadResult::ImageF32(image) => {
+                tmp.fdata = image.data;
+                tmp.image_width = image.width as u32;
+                tmp.image_height = image.height as u32;
+                tmp.bytes_per_pixel = image.depth as u32;
+                tmp.bytes_per_scanline = tmp.image_width * tmp.bytes_per_pixel;
+                tmp.convert_to_bytes();
+            }
+        }
+        tmp
+    }
+
+    pub fn float_to_byte(value: f32) -> u8 {
+        if value <= 0.0 {
+            return 0;
+        }
+        if value >= 1.0 {
+            return 255;
+        }
+        return (256.0 * value) as u8;
+    }
+
+    pub fn byte_to_float(value: u8) -> f32 {
+        value as f32 / 255.0
+    }
+
+    pub fn convert_to_bytes(&mut self) {
+        self.bdata.clear();
+
+        let total_bytes = self.image_height * self.image_width * self.bytes_per_pixel;
+        self.bdata.reserve(total_bytes.try_into().unwrap());
+        self.fdata.iter().for_each(|f| {
+            self.bdata.push(Self::float_to_byte(*f));
+        });
+    }
+    pub fn convert_to_floats(&mut self) {
+        self.fdata.clear();
+
+        let total_bytes = self.image_height * self.image_width * self.bytes_per_pixel;
+        self.fdata.reserve(total_bytes.try_into().unwrap());
+        self.bdata.iter().for_each(|f| {
+            self.fdata.push(Self::byte_to_float(*f));
+        });
+    }
+
+    pub fn pixel_data(&self, mut x: u32, mut y: u32) -> [u8; 3] {
+        x = x.clamp(0, self.image_width - 1);
+        y = y.clamp(0, self.image_height - 1);
+
+        let index = (y * self.bytes_per_scanline + x * self.bytes_per_pixel) as usize;
+        self.bdata[index..index + 3]
+            .try_into()
+            .unwrap_or([255, 0, 255])
+    }
+}
--- src/texture.rs
+++ src/texture.rs
@@ -1,9 +1,9 @@
-use crate::vec3::Vec3;
+use crate::{interval::Interval, rtw_image::RTWImage, vec3::Vec3};
 
 #[derive(Debug)]
 pub enum Texture {
     Color(Vec3),
-    Image(),
+    Image(RTWImage),
     CheckerBoard {
         inv_scale: f64,
         a: Box<Texture>,
@@ -15,13 +15,34 @@ impl Texture {
     pub fn value(&self, u: f64, v: f64, point: &Vec3) -> Vec3 {
         match self {
             Texture::Color(color) => return *color,
-            Texture::Image() => todo!(),
-            Texture::CheckerBoard { inv_scale, a, b } => {
-                let x_int = (point.x * inv_scale).floor();
-                let y_int = (point.y * inv_scale).floor();
-                let z_int = (point.z * inv_scale).floor();
+            Texture::Image(image) => {
+                if image.image_height <= 0 {
+                    return Vec3::new(0.0, 1.0, 1.0);
+                }
 
-                let is_even = (x_int + y_int + z_int) as u64 % 2 == 0;
+                let u = Interval::new(0.0, 1.0).clamp(u);
+                let v = 1.0 - Interval::new(0.0, 1.0).clamp(v);
+
+                let i = (u * (image.image_width as f64)) as u32;
+                let j =
+                    (v * (image.image_height as f64)).min(image.image_height as f64 - 1.0) as u32;
+
+                let color = image.pixel_data(i, j);
+
+                let val = Vec3::new(
+                    RTWImage::byte_to_float(color[0]) as f64,
+                    RTWImage::byte_to_float(color[1]) as f64,
+                    RTWImage::byte_to_float(color[2]) as f64,
+                );
+                val
+            }
+
+            Texture::CheckerBoard { inv_scale, a, b } => {
+                let x_int = (point.x * inv_scale).floor() as i64;
+                let y_int = (point.y * inv_scale).floor() as i64;
+                let z_int = (point.z * inv_scale).floor() as i64;
+
+                let is_even = (x_int + y_int + z_int) % 2 == 0;
 
                 if is_even {
                     a.value(u, v, point)
