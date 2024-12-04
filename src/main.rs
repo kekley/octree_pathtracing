8620dddb8cbd0dabec83e47e3663254496d1809c
kekley <111942843+kekley@users.noreply.github.com>
2024-12-04 09:32:42 -0800

    we ball

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use core::str;
 use std::error::Error;
 use std::fs;
 use std::io::{Cursor, Read};
+use std::sync::Mutex;
 use std::{fs::File, io::Write, time::Instant};
 
 use fastrand::Rng;
@@ -17,8 +18,10 @@ use ray_tracing::AABB;
 use ray_tracing::{random_float, random_float_in_range, random_vec};
 use ray_tracing::{BVHTree, TextureManager};
 use ray_tracing::{HitList, Hittable};
-use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator};
-use rayon::vec;
+use rayon::iter::{
+    IndexedParallelIterator, IntoParallelIterator, IntoParallelRefIterator, ParallelIterator,
+};
+use rayon::{result, vec};
 use spider_eye::{Chunk, ChunkData, Region, SpiderEyeError, World};
 
 pub const ASPECT_RATIO: f32 = 1.5;
@@ -223,85 +226,6 @@ fn cube() {
     file.write(&buf[..]).unwrap();
 }
  */
-fn chunk() -> Result<(), Box<dyn Error>> {
-    let mut region_file = File::open("r.0.0.mca").unwrap();
-
-    let mut file_data = Vec::with_capacity(region_file.metadata().unwrap().len() as usize);
-
-    region_file.read_to_end(&mut file_data).unwrap();
-
-    let mut region = Region::from_bytes(file_data)?;
-
-    let mut hitlist = HitList::new();
-    let mut material_manager = TextureManager::new();
-    let mut camera = Camera::new();
-
-    camera.aspect_ratio = 16.0 / 9.0;
-    camera.image_width = 400;
-    camera.samples_per_pixel = 4;
-    camera.max_depth = 10;
-    camera.v_fov = 70.0;
-    camera.look_from = Vec3::new(0.0, -57.0, 0.0);
-    camera.look_at = Vec3::new(10.0, -59.0, 10.0);
-    camera.v_up = Vec3::new(0.0, 1.0, 0.0);
-
-    camera.defocus_angle = 0.0;
-
-    for chunk in region.compresssed_chunks {
-        if let Some(compressed_chunk) = chunk {
-            let chunk_data = Region::decompress_chunk(&compressed_chunk);
-
-            let chunk = Chunk::from_bytes(chunk_data).unwrap();
-            let pos = Vec3::new(chunk.xpos as f32, 0.0 as f32, chunk.zpos as f32);
-            let distance = ((pos * 16.0) - camera.look_from).length();
-            if distance >= 128.0 {
-                continue;
-            }
-            let data = chunk.get_data();
-
-            (-64..320).for_each(|y: i16| {
-                (0..16).for_each(|x: i16| {
-                    (0..16).for_each(|z: i16| {
-                        let chunk_x = chunk.xpos * 16;
-                        let chunk_z = chunk.zpos * 16;
-                        let block = data.get_block(x, y, z);
-                        if !(block.0 == "minecraft:air") {
-                            let start_pos = Vec3::new(
-                                chunk_x as f32 + x as f32,
-                                y as f32,
-                                chunk_z as f32 + z as f32,
-                            );
-
-                            let mat = if (block.0 == "minecraft:grass_block") {
-                                material_manager.get_or_make_material_idx("minecraft:grass_block")
-                            } else {
-                                material_manager
-                                    .get_or_make_material_idx(str::from_utf8(block.0).unwrap())
-                            };
-                            let end_pos = start_pos + 1.0;
-
-                            let block = Cuboid::new(AABB::from_points(start_pos, end_pos), mat);
-                            hitlist.add(Hittable::Box(block));
-                        }
-                    });
-                });
-            });
-        }
-    }
-
-    println!("{:?}", hitlist.objects.len());
-
-    let tree = BVHTree::from_hit_list(&hitlist);
-
-    let buf = camera.multi_threaded_render(&Hittable::BVH(tree), material_manager.clone());
-
-    //file to write to
-    let mut file = File::create("./output.ppm").unwrap();
-
-    file.write(&buf[..]).unwrap();
-
-    Ok(())
-}
 
 fn world() -> Result<(), Box<dyn Error>> {
     let mut hitlist = HitList::new();
@@ -320,7 +244,7 @@ fn world() -> Result<(), Box<dyn Error>> {
     camera.defocus_angle = 0.0;
 
     //world stuff here
-    let world = World::new("./biggerworld");
+    let mut world = World::new("./biggerworld");
 
     let chunk_view_distance: i32 = 500;
     let starting_chunk_x = (camera.look_from.x as i32) >> 4;
@@ -328,48 +252,18 @@ fn world() -> Result<(), Box<dyn Error>> {
     // Estimate the number of regions based on chunk view distance
     let estimated_regions = (chunk_view_distance * chunk_view_distance) as usize;
 
-    let mut regions: Vec<Region> = Vec::with_capacity(100);
-    for x in starting_chunk_x..starting_chunk_x + chunk_view_distance {
-        for z in starting_chunk_z..starting_chunk_z + chunk_view_distance {
-            if let Some(region) = world.get_region_containing_chunk(x, z) {
-                regions.push(region.clone());
-            }
-        }
-    }
-
-    let a = regions
-        .par_iter()
-        .filter(|region| {
-            (((region.x * 32).pow(2) + (region.z * 32).pow(2)) as f32).sqrt()
-                <= chunk_view_distance as f32
-        })
-        .map(|region| {
-            let chunks = region
-                .compresssed_chunks
-                .clone()
-                .par_iter()
-                .filter_map(|opt| {
-                    if let Some(compressed_data) = opt {
-                        let decompressed = Region::decompress_chunk(&compressed_data);
-                        let chunk = Chunk::from_bytes(decompressed).unwrap();
-                        let dist = (((chunk.xpos * 16) as f32 - camera.look_from.x).powi(2)
-                            + ((chunk.zpos * 16) as f32 - camera.look_from.z).powi(2))
-                        .sqrt();
-                        if dist <= chunk_view_distance as f32 {
-                            return Some(chunk);
-                        } else {
-                            return Some(chunk);
-                        }
-                    } else {
-                        return None;
-                    }
-                })
-                .collect::<Vec<_>>();
-            chunks
-        })
-        .collect::<Vec<_>>();
-
-    println!("{:?}", a.len());
+    let result = Vec::with_capacity(200);
+    let mutex = Mutex::new(result);
+    (-100..100).for_each(|z| {
+        let a: Vec<_> = (-100..100)
+            .into_par_iter()
+            .filter_map(|x| world.get_chunk(x, z))
+            .collect();
+        let mut result = mutex.lock().unwrap();
+        result.extend(a);
+    });
+    let result = mutex.into_inner().unwrap();
+    println!("{:?}", result.len());
     //let tree = BVHTree::from_hit_list(&hitlist);
 
     //let buf = camera.multi_threaded_render(&Hittable::BVH(tree), material_manager.clone());
